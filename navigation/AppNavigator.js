1eea2e0f8ef255acbb23261e4f80a1e0146e8a28
Dale Watson <dellryuzi@gmail.com>
2019-02-13 20:09:29 +0700

    search query and fetch data

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import HomeScreen from '../screens/HomeScreen'
 
-import MainTabNavigator from './MainTabNavigator';
+// import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(createSwitchNavigator({
-  
+  Main: HomeScreen 
   // Main: MainTabNavigator,
 }));
